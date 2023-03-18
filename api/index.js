2e86aa27da1e9721f969367218b254883b8690be
Daniel Martinez <daniellhf@icloud.com>
2023-03-18 17:39:50 -0400

    hasBBG true case

--- api/index.js
+++ api/index.js
@@ -5,25 +5,13 @@ const port = 3001;
 require("dotenv").config({ path: require("find-config")(".env") });
 const cors = require("cors");
 app.use(cors());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
-
 const dialogExample = [
-  {
-    speaker: "user",
-    text: "Hello, how are you?",
-  },
+  { speaker: "user", text: "Hello, how are you?" },
   {
     speaker: "bot",
     text: "I am doing well, thank you. How can I help you today?",
@@ -31,34 +19,10 @@ const dialogExample = [
 ];
 
 async function freeRequest(req) {
-  
   let conversationBody = "";
-
-  const messagesTest = [
-     {
-      "_id": 1,
-      "createdAt": '2023-03-18T20:48:42.322Z',
-      "text": "Hey Daniel! My name's Gorg! I'm your personal chat buddy, how are you?",
-      "user":  {
-        "_id": 5,
-        "avatar": 8,
-        "name": "Gorg",
-      },
-    },
-     {
-      "_id": "4e061363-f6c0-4893-88b7-5e43e2c5c705",
-      "createdAt": '2023-03-18T20:48:47.392Z',
-      "text": "Hey gorg",
-      "user":  {
-        "_id": 1,
-      },
-    },
-  ]
-
   req.messages.forEach((messageNode) => {
     if (messageNode.user._id == 5) {
-      //message is from ai
-      conversationBody += "\nGorg:";
+      //message is from ai conversationBody += "\nGorg:";
       conversationBody += messageNode.text;
     } else {
       //message is from user
@@ -67,7 +31,6 @@ async function freeRequest(req) {
       conversationBody += " \nGorg:&nbsp;";
     }
   });
-
   const name = req.name;
   const prompt = `The following is a conversation between you, an AI chat buddy named Gorg and a human ${name}. The buddy is helpful, creative, clever, very friendly and applies psychology to help the human, however does not under any circumstances provide medical advice, talk about treatment, or give medical information, or talk about sexual topics.`;
   const promptToSend = prompt + conversationBody;
@@ -84,6 +47,46 @@ async function freeRequest(req) {
   return { result: completion.data.choices[0].text };
 }
 
+async function BBGRequest(req) {
+  const messagesTest = [
+    {
+      _id: 1,
+      createdAt: "2023-03-18T20:48:42.322Z",
+      text: "Hey Daniel! My name's Gorg! I'm your personal chat buddy, how are you?",
+      user: { _id: 5, avatar: 8, name: "Gorg" },
+    },
+    {
+      _id: "4e061363-f6c0-4893-88b7-5e43e2c5c705",
+      createdAt: "2023-03-18T20:48:47.392Z",
+      text: "Hey gorg",
+      user: { _id: 1 },
+    },
+  ];
+
+  let conversationMessages = [];
+  messagesTest.forEach((messageNode) => {
+    if (messageNode.user._id == 5) {
+      //message is from ai
+      conversationMessages.push({
+        role: "assistant",
+        content: messageNode.text,
+      });
+    } else {
+      //message is from user
+      conversationMessages.push({ role: "user", content: messageNode.text });
+    }
+  });
+
+  const name = req.name;
+  const systemPrompt = `The following is a conversation between you, an AI chat buddy named Gorg and a human ${name}. The buddy is helpful, creative, clever, very friendly and applies psychology to help the human, however does not under any circumstances provide medical advice, talk about treatment, or give medical information, or talk about sexual topics.`;
+  conversationMessages.unshift({ role: "system", content: systemPrompt });
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: conversationMessages,
+  });
+  return { result: completion.data.choices[0].message };
+}
+
 app.get("/api", (req, res) => {
   request(req.query.prompt)
     .then((result) => {
@@ -98,9 +101,16 @@ app.get("/api", (req, res) => {
 app.post("/api", (req, res) => {
   console.log(req.body);
   let output = null;
-  // res.send("hi")
-  // let output = request().then((result) => console.log(result))
+  // res.send("hi") // let output = request().then((result) => console.log(result))
   if (req.body.hasBBG) {
+    BBGRequest(req.body)
+      .then((result) => {
+        res.json(result);
+      })
+      .then((data) => {
+        output = data;
+        console.log(data);
+      });
   } else {
     freeRequest(req.body)
       .then((result) => {
@@ -112,7 +122,6 @@ app.post("/api", (req, res) => {
       });
   }
 });
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
