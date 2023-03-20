6f4b914e1aae6563cf9b19e5d215d97a9f04ef67
Daniel Martinez <daniellhf@icloud.com>
2023-03-19 20:34:23 -0400

    with name in response

--- api/index.js
+++ api/index.js
@@ -118,7 +118,7 @@ app.post("/api", (req, res) => {
       totalWords += item.text.split(" ").length;
     })
     if (totalWords >= 200) {
-      res.json({ result: `Oops! Sorry bud, our conversation is longer than 200 words so sadly I can't keep talking. You can upgrade to BBG to continue talking though!` })
+      res.json({ result: `Oops! Sorry ${req.body.name ? req.body.name : 'bud'}, our conversation is longer than 200 words so sadly I can't keep talking. You can upgrade to BBG to continue talking though!` })
     } else {
       freeRequest(req.body)
       .then((result) => {
